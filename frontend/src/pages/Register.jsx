62d0ae3fd9f3eb1fb608bdb42832b7974dd0690c
Priyanshu Parashar <priyanshuparashar08@gmail.com>
2025-05-18 20:36:48 +0530

    create auth pages

---
+++ frontend/src/pages/Register.jsx
@@ -0,0 +1,9 @@
+const Register = () => {
+  return (
+    <div>
+      <p>in register components!</p>
+    </div>
+  );
+};
+
+export default Register;
--- frontend/src/routes/routes.jsx
+++ frontend/src/routes/routes.jsx
@@ -1,19 +1,29 @@
 import { createBrowserRouter } from "react-router-dom";
 import RootLayout from "../layouts/RootLayout";
 import LandingPage from "../pages/LandingPage";
+import Register from "../pages/Register";
+import Login from "../pages/Login";
 
 const routes = createBrowserRouter([
-    {
-        path: '',
-        element: <RootLayout />,
-        errorElement: <div>Error!</div>,
-        children: [
-            {
-                index: true,
-                element: <LandingPage />
-            }
-        ]
-    }
+  {
+    path: "",
+    element: <RootLayout />,
+    errorElement: <div>Error!</div>,
+    children: [
+      {
+        index: true,
+        element: <LandingPage />,
+      },
+      {
+        path: "register",
+        element: <Register />,
+      },
+      {
+        path: "login",
+        element: <Login />,
+      },
+    ],
+  },
 ]);
 
 export default routes;
